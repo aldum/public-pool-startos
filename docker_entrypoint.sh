932aeffbb55b79d75f1162d69445f68648c1b261
aldum <aldum@artixlinux.org>
2024-03-30 23:24:16 +0100

    feat: init via bash

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -36,7 +36,7 @@ fi
 
 
 ### run
-# exec tini -p SIGTERM -- bitcoind-manager
 /usr/local/bin/node /opt/public-pool/dist/main &
-exec caddy run --config $CADDY_CONFIG
-# TODO proper init/pm
+exec caddy run --config $CADDY_CONFIG &
+
+wait -n
