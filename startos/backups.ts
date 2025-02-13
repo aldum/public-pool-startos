df0b2ed68dcd0b4a608d138fed2021979815836d
aldum <aldum@artixlinux.org>
2025-02-13 21:56:10 +0100

    style: formatting

--- startos/backups.ts
+++ startos/backups.ts
@@ -1,5 +1,6 @@
 import { sdk } from "./sdk"
 
 export const { createBackup, restoreBackup } = sdk.setupBackups(
-  async ({ effects }) => sdk.Backups.volumes("pool"),
+  async ({ effects }) =>
+    sdk.Backups.volumes("pool"),
 )
