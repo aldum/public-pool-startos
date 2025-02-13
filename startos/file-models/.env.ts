4ce605d8093d1d327d08f35213935c40b5fc4244
aldum <aldum@artixlinux.org>
2025-02-13 21:56:10 +0100

    refactor: .env shape literal types

--- startos/file-models/.env.ts
+++ startos/file-models/.env.ts
@@ -1,25 +1,9 @@
 import { FileHelper, matches } from "@start9labs/start-sdk"
 import { dotenvToJson, jsonToDotenv } from "./fmutils"
+import { apiPort, stratPort } from "../utils"
 
-const { object, string } = matches
-// const { anyOf, literal } = matches
-// const stringArray = matches.array(matches.string)
-// const string = stringArray.map(([a]) => a)
-// const number = stringArray.map(([a]) => Number(a))
-// const bool = anyOf(literal("true"), literal("false"))
+const { object, string, literal } = matches
 
-// export type ppEnv = {
-//   BITCOIN_RPC_URL: "http://bitcoind.startos"
-//   BITCOIN_RPC_PORT: number
-//   BITCOIN_RPC_TIMEOUT: number
-//   API_PORT: number
-//   STRATUM_PORT: number
-//   NETWORK: string
-//   API_SECURE: boolean
-//   ENABLE_SOLO: boolean
-//   ENABLE_PROXY: boolean
-//   BITCOIN_RPC_COOKIEFILE: string
-// }
 export type ppEnv = {
   BITCOIN_RPC_URL: string
   BITCOIN_RPC_PORT: string
@@ -36,22 +20,21 @@ export type ppEnv = {
 }
 
 const shape = object({
-  BITCOIN_RPC_URL: string,
-  BITCOIN_RPC_PORT: string,
+  BITCOIN_RPC_URL: literal("http://bitcoind.startos"),
+  BITCOIN_RPC_PORT: literal("8332"),
   BITCOIN_RPC_TIMEOUT: string,
-  API_PORT: string,
-  STRATUM_PORT: string,
-  NETWORK: string,
-  API_SECURE: string,
-  ENABLE_SOLO: string,
-  ENABLE_PROXY: string,
+  API_PORT: literal(apiPort.toString()),
+  STRATUM_PORT: literal(stratPort.toString()),
+  NETWORK: literal("mainnet"),
+  API_SECURE: literal("false"),
+  ENABLE_SOLO: literal("true"),
+  ENABLE_PROXY: literal("false"),
   BITCOIN_RPC_USER: string,
   BITCOIN_RPC_PASSWORD: string,
   // BITCOIN_RPC_COOKIEFILE: string.optional(),
 })
 
 export const pubpoolEnvFile = FileHelper.raw(
-  // "/public-pool/.env" + "fm",
   "/media/startos/volumes/pool/cfg/.env",
   jsonToDotenv<ppEnv>,
   dotenvToJson<ppEnv>,
