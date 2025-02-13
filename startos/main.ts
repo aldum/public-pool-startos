39d5a2767bf153eeb7d0f45cfaa49045a6802fda
aldum <aldum@artixlinux.org>
2025-02-13 21:56:10 +0100

    feat: prestart frontend container

--- startos/main.ts
+++ startos/main.ts
@@ -49,15 +49,36 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
     requires: [],
   })
 
+  const frontend = await sdk.SubContainer.of(effects,
+    { imageId: "frontend" },
+    "frontend"
+  )
+  frontend.mount(
+    {
+      type: "assets",
+      id: "frontend",
+      subpath: null,
+    },
+    "/assets"
+  )
+  frontend.mount(
+    {
+      type: "volume",
+      id: "pool",
+      subpath: "env",
+      readonly: false
+    },
+    "/var/www/html/env"
+  )
+
   daemons.addDaemon("frontend", {
-    subcontainer: { imageId: "frontend" },
+    subcontainer: frontend,
     command: ["/bin/sh", "/assets/entrypoint.sh"],
     env: {
       HOME: "/home",
     },
     mounts: sdk.Mounts.of()
-      .addAssets("frontend", null, "/assets")
-      .addVolume("pool", "env", "/var/www/html/env", true),
+    ,
     ready: {
       display: "Web Interface",
       fn: () =>
