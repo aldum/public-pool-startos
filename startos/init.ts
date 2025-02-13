cbc180fac0f3ee3e3de842a64e64ee93f8878d09
aldum <aldum@artixlinux.org>
2025-02-13 10:04:30 +0100

    style: clean init.ts

--- startos/init.ts
+++ startos/init.ts
@@ -4,20 +4,14 @@ import { setDependencies } from "./dependencies"
 import { setInterfaces } from "./interfaces"
 import { versions } from "./versions"
 import { actions } from "./actions"
-// import { utils } from '@start9labs/start-sdk'
-// import { setPrimaryUrl } from './actions/set-primary-url'
 import { setStratumUrl } from "./actions/set-stratum-url"
 
 // **** Install ****
 const install = sdk.setupInstall(async ({ effects }) => {
-  console.info("━━━━━ init start ━━━━━")
-  // await sdk.store.setOwn(effects, sdk.StorePath, {
-  //   PUBPOOL__server__ROOT_URL: '',
-  // })
   await sdk.action.requestOwn(effects, setStratumUrl, "important")
 })
 // **** Uninstall ****
-const uninstall = sdk.setupUninstall(async () => {})
+const uninstall = sdk.setupUninstall(async () => { })
 
 /**
  * Plumbing. DO NOT EDIT.
