367e36ede384c91f2f067c6b055e915e44c2aff3
aldum <aldum@artixlinux.org>
2025-02-13 21:56:10 +0100

    fix: change btcd healthcheck for 036

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -5,8 +5,7 @@ export const setDependencies = sdk.setupDependencies(async ({ effects }) => {
     bitcoind: {
       kind: "running",
       versionRange: ">=27.1.0",
-      // healthChecks: ["primary"],
-      healthChecks: ["rpc"],
+      healthChecks: ["primary"],
     },
   }
 })
