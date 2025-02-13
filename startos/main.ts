ba47af474672947e3db2489d593449ef9b13c96b
aldum <aldum@artixlinux.org>
2025-02-13 21:56:10 +0100

    refactor: rename pubpool .env type

--- startos/main.ts
+++ startos/main.ts
@@ -2,7 +2,7 @@ import { sdk } from "./sdk"
 import { T } from "@start9labs/start-sdk"
 import { apiPort, stratPort, uiPort } from "./utils"
 import { manifest as btcManifest } from "bitcoind-startos/startos/manifest"
-import { ppEnv } from "./file-models/_env"
+import { ppEnv } from "./file-models/pubPoolEnv"
 
 export const main = sdk.setupMain(async ({ effects, started }) => {
   console.info(
